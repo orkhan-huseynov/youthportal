86a15db33256c3a4becd3e1c7a0aa0c698a25b08
Nigar <niko.kazim@gmail.com>
2018-02-09 00:45:10 +0400

    lang button

--- resources/views/news_details.blade.php
+++ resources/views/news_details.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
+@section('search_input')
+    <div class="col-sm-12 col-md-6 search_container">
+        <div class="input-group mb-3 search_btn">
+            <input type="search" class="form-control" placeholder="@if ($lang == 'az') axtar @else поиск @endif" aria-label="search" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button"><i class="fa fa-search search_btn_icon" aria-hidden="true"></i></button>
+            </div>
+        </div>
+    </div>
+@endsection
 @section('main_menu')
     <div class="collapse navbar-collapse menu_inner" id="navbarNav">
         <ul class="navbar-nav">
@@ -85,5 +95,50 @@
                 </div>
             @endforeach
         </div>
+    </div>
+@endsection
+
+@section('footer')
+    <footer>
+        <div class="container-fluid">
+            <div class="row footer_inner">
+                {{--<div class="col-sm-12 col-md-3 hover_class footer_category">
+                    <a href="#" class="footer_a"><p class="footer_p"><span class="news_category_span">TWEETS</span></p></a>
+                </div>--}}
+                <div class="col-sm-12 col-md-6 hover_class footer_category">
+                    <p class="footer_p footer_a"><span class="news_category_span">@if($lang == 'az') NAVİQASİYA @else НАВИГАЦИЯ @endif</span></p>
+                    <ul class="footer_ul">
+                        @foreach($sections as $section)
+                            <li><a href="{{url('section/'.$section->id)}}"><i class="fa fa-chevron-right" aria-hidden="true"></i><span>  @if($lang == 'ru') {{' '.$section->name_ru}} @else {{' '.$section->name_az}} @endif</span></a></li>
+                        @endforeach
+                    </ul>
+                </div>
+                {{--<div class="col-sm-12 col-md-3 hover_class footer_category">
+                    <a href="#" class="footer_a"><p class="footer_p"><span class="news_category_span">SOMETHING</span></p></a>
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4 footer_img_container">
+                            <img src="{{ asset('stock-photo-financial-business-news.jpg') }}" class="footer_img" alt="footer photo"/>
+                            <img src="{{ asset('images/tn_ir-iran-russia-mou-20171218.jpg') }}" class="footer_img" alt="footer photo"/>
+                            <img src="{{ asset('images/7deb467507756b82a9cfeb84d6a27aab.jpg') }}" class="footer_img" alt="footer photo"/>
+                        </div>
+                        <div class="col-sm-6 col-md-4 footer_img_container">
+                            <img src="{{ asset('images/SILO_fb_022117_stockNews_shutter.jpg') }}" class="footer_img" alt="footer photo"/>
+                            <img src="{{ asset('images/man-coffee-cup-pen.jpg') }}" class="footer_img" alt="footer photo"/>
+                            <img src="{{ asset('images/mother-holding-baby-landing.jpg') }}" class="footer_img" alt="footer photo"/>
+                        </div>
+                        <div class="col-sm-6 col-md-4 footer_img_container">
+                            <img src="{{ asset('images/micex-russia-stocks-ruble.jpg') }}" class="footer_img" alt="footer photo"/>
+                            <img src="{{ asset('images/blue-apron-shares-make-bland-debut-2017-6.jpg') }}" class="footer_img" alt="footer photo"/>
+                            <img src="{{ asset('images/stock-photo-financial-business-news-online-on-a-laptop-with-coffee-and-stationery-252720643.jpg') }}" class="footer_img" alt="footer photo"/>
+                        </div>
+                    </div>
+                </div>--}}
+                <div class="col-sm-12 col-md-6 hover_class footer_category">
+                    <p class="footer_p footer_a"><span class="news_category_span">@if($lang == 'az') HAQQIMIZDA @else О НАС @endif</span></p>
+                    <p class="footer_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
     </div>
 @endsection
